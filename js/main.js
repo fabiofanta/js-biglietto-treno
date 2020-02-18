11ace4b271d55a9d4231a333ab009b75dbd1b8a5
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-18 16:58:26 +0100

    3-rel

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@
 var kmRequest = prompt("Quanti chilometri vuoi percorrere?");
 var ageRequest = prompt("Quanti anni hai?");
 var priceXKm = 0.21;
+var over65 = 0.4;
+var under18 = 0.2;
 
 if (ageRequest < 18) {
-    var totalprice = 0.8 * kmRequest * priceXKm;
+    var totalprice = (1 - under18) * kmRequest * priceXKm;
 } else if  (ageRequest > 65) {
-    var totalprice = 0.6 * kmRequest * priceXKm;
+    var totalprice = (1 - over65) * kmRequest * priceXKm;
 }else {
     var totalprice = kmRequest * priceXKm;
 
