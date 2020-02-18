f29b7f3c77e15d65940461e486410a380bb2b0f0
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-18 16:50:01 +0100

    1-rel

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// richiesta numero km da percorrere
+// richiesta età del passeggero
+// moltiplicare i km dichiarati con il prezzo del biglietto al km
+// se < 18 moltiplicare per 0.8, se > 65 moltiplicare per 0.4
+
+var kmRequest = prompt("Quanti chilometri vuoi percorrere?");
+var ageRequest = prompt("Quanti anni hai?");
+var priceXKm = 0.21;
+
+if (ageRequest < 18) {
+    var totalprice = 0.8 * kmRequest * priceXKm;
+} else if  (ageRequest > 65) {
+    var totalprice = 0.4 * kmRequest * priceXKm;
+}else {
+    var totalprice = kmRequest * priceXKm;
+
+}
+console.log(totalprice);
